5abff2ff7c07e18ed5ccc029bcc2e54bf4507d59
ngthu1995 <ngthu1995@gmail.com>
2024-06-03 05:38:18 -0400

    Updates

---
+++ precache-manifest.04207a4911cc48d1c1bb08cf088f696e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "93d0b0afb1554fd71681",
+    "url": "/bloc-jams-react/static/css/main.d67b7379.chunk.css"
+  },
+  {
+    "revision": "93d0b0afb1554fd71681",
+    "url": "/bloc-jams-react/static/js/main.93d0b0af.chunk.js"
+  },
+  {
+    "revision": "4e428cb3038d57c0c97b",
+    "url": "/bloc-jams-react/static/js/1.4e428cb3.chunk.js"
+  },
+  {
+    "revision": "8532c21bd7bbf1dd1b0c",
+    "url": "/bloc-jams-react/static/js/runtime~main.8532c21b.js"
+  },
+  {
+    "revision": "7efce81d851244fcdb6ba397b4f4fe97",
+    "url": "/bloc-jams-react/static/media/spaceship.7efce81d.png"
+  },
+  {
+    "revision": "7cfbe5aa9d98c60d87c487d6ec3cd0dd",
+    "url": "/bloc-jams-react/static/media/dog-landingpage.7cfbe5aa.jpg"
+  },
+  {
+    "revision": "244bd7653b81f80a21a036a6b6e1257e",
+    "url": "/bloc-jams-react/static/media/landingpage.244bd765.jpg"
+  },
+  {
+    "revision": "dd50049fe13827b20f7fbf1ca41cd70d",
+    "url": "/bloc-jams-react/index.html"
+  }
+];
